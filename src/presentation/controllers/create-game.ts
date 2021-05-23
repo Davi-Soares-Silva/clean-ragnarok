f811705d22bb4e2f2e11f7b391217516b4956209
Davi-Soares-Silva <davisoares4456@gmail.com>
2021-05-23 17:11:45 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/create-game.ts
+++ src/presentation/controllers/create-game.ts
@@ -1,20 +1,15 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { HttpRequest, HttpResponse } from '../protocols/http'
+import { badRequest } from '../helpers/bad-request'
 
 export class CreateGameController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
 
     if (!httpRequest.body.price) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('price')
-      }
+      return badRequest(new MissingParamError('price'))
     }
 
     return {
