72f33b772659ce5655d272086647a082922dec7b
Davi-Soares-Silva <davisoares4456@gmail.com>
2021-05-23 16:38:19 -0300

    test: ensure the CreateGameController returns 400 if no price is provided

--- src/presentation/controllers/create-game.spec.ts
+++ src/presentation/controllers/create-game.spec.ts
@@ -15,4 +15,19 @@ describe('CreateGame Controller', () => {
     expect(httResponse.statusCode).toBe(400)
     expect(httResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('should return 400 if no price is provided', () => {
+    const sut = new CreateGameController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        description: 'any_description',
+        imageUrl: 'http://any_url.com'
+      }
+    }
+    const httResponse = sut.handle(httpRequest)
+
+    expect(httResponse.statusCode).toBe(400)
+    expect(httResponse.body).toEqual(new Error('Missing param: price'))
+  })
 })
