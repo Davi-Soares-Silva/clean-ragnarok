a1db5de887fdc329d39e42bfc77294b63f941f58
Davi-Soares-Silva <davisoares4456@gmail.com>
2021-05-30 21:32:42 -0300

    feat: ensure the RegisterUserController crypt password before register

--- src/data/usecases/db-register-user.ts
+++ src/data/usecases/db-register-user.ts
@@ -1,22 +1,26 @@
 import { RegisterUser } from '../../domain/usecases/register-user';
+import { Hasher } from '../protocols/cryptography/hasher';
 import { AddUserRepository } from '../protocols/db/add-user-repository';
+import { CheckUserByEmailRepository } from '../protocols/db/check-user-by-email-repository';
 
 export class DbRegisterUser implements RegisterUser {
   constructor(
-    private readonly addUserRepository: AddUserRepository
+    private readonly hasher: Hasher,
+    private readonly addUserRepository: AddUserRepository,
+    private readonly checkUserByEmailRepository: CheckUserByEmailRepository
   ) {}
 
-  public async register ({
-    name,
-    email,
-    password,
-    userTypeId,
-  }: AddUserRepository.Params): AddUserRepository.Result {
+  public async register (data: AddUserRepository.Params): AddUserRepository.Result {
+
+    const userExists = await this.checkUserByEmailRepository.checkByEmail(data.email);
+
+    if(userExists) throw new Error('EMAIL_ALREADY_EXISTS')
+
+    const hashedPassword = await this.hasher.hash(data.password);
+
     const user = await this.addUserRepository.add({
-      name,
-      email,
-      password,
-      userTypeId,
+      ...data,
+      password: hashedPassword,
     });
 
     if(!user) throw new Error('ERROR_CREATING_USER');
