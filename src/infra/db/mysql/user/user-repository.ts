0a4c6cdd28cd8fac751ca66ba8a9af5031ef89da
Davi-Soares-Silva <davisoares4456@gmail.com>
2021-06-01 21:47:21 -0300

    feat: init the authentication flow

--- src/infra/db/mysql/user/user-repository.ts
+++ src/infra/db/mysql/user/user-repository.ts
@@ -12,7 +12,6 @@ export class UserRepository implements AddUserRepository, CheckUserByEmailReposi
       ...data,
     }
 
-    delete createdUser.password;
     return formateCamelCaseKeysForSnakeCase(createdUser);
   }
 
